cc11e3c06b699976776b09c471e1a9018754a8f5
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-19 17:53:13 -0800

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './errorPage.jsx'
+import Quiz, { loader as quizLoader } from './routes/quiz.jsx'
+import Index from './routes/index.jsx'
+import Root from './routes/root.jsx'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: 'quiz/:quizId',
+        element: <Quiz />,
+        loader: quizLoader
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
---
+++ src/routes/quiz.jsx
@@ -0,0 +1,11 @@
+export const loader = ({ params }) => params
+
+const Quiz = () => {
+  return (
+    <div>
+      Quiz
+    </div>
+  )
+}
+
+export default Quiz
