e025495e23f89bfacedc5951d8eaca504d65bb5b
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-19 19:03:12 -0800

    basic qa logic

--- src/routes/quiz.jsx
+++ src/routes/quiz.jsx
@@ -1,48 +1,89 @@
 import { useLoaderData } from 'react-router-dom'
+import quizzes from '../data.json'
+import { useState } from 'react'
 
-export const loader = ({ params }) => params
+export const loader = ({ params }) => {
+  const { quizId } = params
+  return quizzes.find(q => q.id === Number(quizId))
+}
 
 const Quiz = () => {
+  const [qIndex, setQIndex] = useState(0)
+  const [chosenAnswerIndex, setChosenAnswerIndex] = useState()
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
   const quiz = useLoaderData()
+  const { questions } = quiz
+  const question = questions[qIndex]
+  const correctAnswerIndex = question.options.findIndex(option => question.answer === option)
+
+  const submitAnswer = e => {
+    if (isSubmitted) return
+    setIsSubmitted(true)
+
+    setTimeout(() => {
+      setQIndex(qIndex + 1)
+      setChosenAnswerIndex(undefined)
+      setIsSubmitted(false)
+    }, 2000)
+    e.preventDefault()
+  }
+
+  const selectOption = i => e => {
+    e.preventDefault()
+    setChosenAnswerIndex(i)
+  }
+
+  const realIndex = qIndex + 1;
+  console.log(realIndex)
+  const completionPercentage = (realIndex / questions.length) * 100
+  console.log(completionPercentage)
+
+  const classListForOption = i => {
+    const classList = []
+    if (chosenAnswerIndex === i) {
+      classList.push('selected')
+    }
+
+    if (isSubmitted && i === correctAnswerIndex) {
+      classList.push('correct')
+    }
+
+    if (isSubmitted && i === chosenAnswerIndex && i !== correctAnswerIndex) {
+      classList.push('incorrect')
+    }
+
+    return classList.join(' ')
+  }
 
   return (
     <>
       <div id="intro">
-        <i>Question 6 of 10</i>
+        <i>Question {realIndex} of {questions.length}</i>
 
         <h2>
-          Which of these color contrast ratios defines the minimum WCAG 2.1 Level AA requirement for normal text?
+          {question.question}
         </h2>
 
         <div id="progressWrapper">
-          <div id="progressBar" style={{width: '40%'}}></div>
+          <div id="progressBar" style={{width: completionPercentage + '%'}}></div>
         </div>
       </div>
 
       <ol id="selectionList" className="questions">
-        <li>
-          <a className="card" href="#">
-            4.5 : 1
-          </a>
-        </li>
-        <li>
-          <a className="card" href="#">
-            3 : 1
-          </a>
-        </li>
-        <li>
-          <a className="card" href="#">
-            2.5 : 1
-          </a>
-        </li>
-        <li>
-          <a className="card" href="#">
-            5 : 1
-          </a>
-        </li>
+        {question.options.map((option, i) => (
+          <li
+            key={i}
+            className={classListForOption(i)}
+          >
+            <a className="card" href="#" onClick={selectOption(i)}>
+              {option}
+            </a>
+          </li>
+        ))}
       </ol>
 
-      <button>Submit Answer</button>
+      <button onClick={submitAnswer}>Submit Answer</button>
     </>
   )
 }
